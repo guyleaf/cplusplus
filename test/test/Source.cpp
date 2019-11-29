190bb0bd5e423a3dc6135856d093b86b60eb5c41
guyleaf <t107590037@ntut.org.tw>
2019-11-29 11:39:04 +0800

    Solve order problem

--- test/test/Source.cpp
+++ test/test/Source.cpp
@@ -1,7 +1,25 @@
+#include <string>
 #include <iostream>
-#include <cstring>
 using namespace std;
+class Exam
+{
+public:
+    Exam() : s("") {};
+    Exam(int s) : s(to_string(s)) {};
+    Exam(string s) : s(s) {};
+    const Exam operator++(int flag)
+    {
+        string t = s;
+        s += "+";
+        return t;
+    }
+private:
+    string s;
+};
+
 int main()
 {
+    Exam t(11);
+    
     return 0;
 }
