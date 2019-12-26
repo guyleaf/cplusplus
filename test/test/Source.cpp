1cb714eb284293ad20d0ef36f52071764a64192b
guyleaf <43506966+guyleaf@users.noreply.github.com>
2019-12-26 14:20:06 +0800

    First commit

--- test/test/Source.cpp
+++ test/test/Source.cpp
@@ -1,35 +1,99 @@
 #include <string>
+#include<vector>
+#include <list>
+#include<algorithm>
 #include <iostream>
 using namespace std;
-class Test
+
+class Animal
 {
 public:
-    Test(): s("")
+    Animal(string name)
     {
-        cout << "default constructor" << endl;
+        this->name = name;
+        legs = 4;
     }
-    /*Test(string& s) : s(s)
+    int getLegs()
     {
-        cout << "copy constructor" << endl;
+        return legs;
+    }
+    void setLegs(int l)
+    {
+        legs = l;
+    }
+    string getName()
+    {
+        return name;
+    }
+    virtual string sound()
+    {
+        return "no sound";
+    }
+    string getAnimal()
+    {
+        cout << this->getName() << endl;
+        return name + ": " + to_string(legs) + " legs / " + sound();
+    }
+protected:
+    string name;
+private:
+    int legs;
+};
+class Dog : public Animal
+{
+public:
+    Dog(string color) : Animal("Dog")
+    {
+        this->color = color;
+    }
+    string sound()
+    {
+        return "bark";
+    }
+    /*int getTotalLegs(Animal& a)
+    {
+        return a.legs + legs;
     }*/
-    Test(string s) : s(s)
+    string getColor()
     {
-        cout << "default constructor s" << endl;
-    }
-    Test do1(int t)
-    {
-        Test t2 = Test("");
-        return t2;
+        return color;
     }
 private:
-    string s;
+    string color;
+};
+class Chicken : public Animal
+{
+public:
+    Chicken() : Animal("Chicken"), wings(2)
+    {
+        setLegs(2);
+    }
+    string sound()
+    {
+        return "bok";
+    }
+    int getWings()
+    {
+        return wings;
+    }
+    string getTotalName(Animal& a)
+    {
+        return name + " " + a.getName();
+    }
+    bool isAfraidOf(Animal a)
+    {
+        return a.getName() == "Dog";
+    }
+    string getAnimal()
+    {
+        return Animal::getAnimal() + " / " + to_string(wings) + " wings";
+    }
+private:
+    int wings;
 };
 
-int main()
+void main()
 {
-    Test t;
-    t = t.do1(5);
-    Test t1(t);
-    system("PAUSE");
-    return 0;
+    Dog dog("yellow");
+    cout << dog.getAnimal() << endl;
 }
